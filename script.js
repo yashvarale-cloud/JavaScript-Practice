f5d1db36661a7df7dde01c21031bb650c416a5dc
yashvarale-cloud <yashvarale@gmail.com>
2026-06-18 15:04:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const profile = {
+    name :"Yash",
+    isFollowed : false,
+    postCount : 195,
+    follwersCount : 569,
+    followingCount : 4,
+    Expeince : "2 years",
+    about : "I am a web developer and designer. I have been working in this field for 2 years. I have a lot of experience in web development and design. I have worked on many projects and have a lot of experience in this field. I am a very passionate person and I love to learn new things. I am always looking for new opportunities to learn and grow in this field."
+    
+};
+console.log(profile);
