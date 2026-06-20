cf8916bb5c724769ad2f9b9bbeb9ec538bd3c469
yashvarale-cloud <yashvarale@gmail.com>
2026-06-20 23:14:18 +0530

    Arryas methods

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-let prices=[250,645,300,900,50]
+// let prices=[250,645,300,900,50]
 
-for(let i=0;i<prices.length;i++){
-    let offer = prices[i]/10;
-    prices[i]=prices[i]-offer;
-}
-console.log(prices)
-prompt("The discounted prices are: " + prices.join(", "));
+// for(let i=0;i<prices.length;i++){
+//     let offer = prices[i]/10;
+//     prices[i]=prices[i]-offer;
+// }
+// console.log(prices)
+
+companies=["Bloomberg","Microsoft","Uber","Google","Apple","IBM"]
+companies.shift()
+companies.splice(2,1,"Ola")
+companies.push("Amazon")
+console.log(companies)
+
