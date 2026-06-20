101a1f2527abe627903c9a8e8b94918919f13407
yashvarale-cloud <yashvarale@gmail.com>
2026-06-20 23:43:54 +0530

    Functions

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-// let prices=[250,645,300,900,50]
+// // let prices=[250,645,300,900,50]
 
-// for(let i=0;i<prices.length;i++){
-//     let offer = prices[i]/10;
-//     prices[i]=prices[i]-offer;
-// }
-// console.log(prices)
+// // for(let i=0;i<prices.length;i++){
+// //     let offer = prices[i]/10;
+// //     prices[i]=prices[i]-offer;
+// // }
+// // console.log(prices)
 
-companies=["Bloomberg","Microsoft","Uber","Google","Apple","IBM"]
-companies.shift()
-companies.splice(2,1,"Ola")
-companies.push("Amazon")
-console.log(companies)
+// companies=["Bloomberg","Microsoft","Uber","Google","Apple","IBM"]
+// companies.shift()
+// companies.splice(2,1,"Ola")
+// companies.push("Amazon")
+// console.log(companies)
 
+function countVowels(str){
+    let count = 0;
+    for(let i=0;i<str.length;i++){
+        if(str[i]=="a" || str[i]=="e" || str[i]=="i" || str[i]=="o" || str[i]=="u"){
+            count++;
+        }
+    }
+    return count;
+}
+let result = countVowels("lorem ipsum dolor sit amet");
+console.log(result)
